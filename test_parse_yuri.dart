70a1d5fb4ffb46cd86968f247e8e5526e784140f
Hardik Bansal <bansalhardik31@gmail.com>
2026-06-01 16:29:48 +0530

    app name change

--- test_parse_yuri.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter_video/features/metadata/filename_parser.dart';
-
-void main() {
-  final name = "[YURI] Love, Chunibyo & Other Delusions!  S02E08 [BD 1080p x264 10bit FLAC].mkv";
-  final parsed = FilenameParser.parse(name);
-  debugPrint(parsed.toString());
-}
--- test_parser2.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter_video/features/metadata/filename_parser.dart';
-
-void main() {
-  final working = "[SubsPlease] Tengoku Daimakyou - 13 (1080p) [2B9B9B8A].mkv";
-  final parsed = FilenameParser.parse(working);
-  debugPrint(parsed.toString());
-}
